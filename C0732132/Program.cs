2125caceb0b3428c991ebdcb9c76eb828843656c
visualstudio <visualstudio@122-PC35>
2019-01-18 20:49:31 -0500

    playing_with_arrays

--- C0732132/Program.cs
+++ C0732132/Program.cs
@@ -10,6 +10,39 @@ namespace C0732132
     {
         static void Main(string[] args)
         {
+            Studentinformation _Mary = new Studentinformation("Mary", 3.8, 20);
+            Studentinformation _john = new Studentinformation("john", 3.8, 20);
+            Studentinformation _Mark = new Studentinformation("Mark", 3.8, 20);
+            Studentinformation _Steven = new Studentinformation("Steven", 3.8, 20);
+            Studentinformation _Cindy = new Studentinformation("Cindy", 3.8, 20);
         }
+
     }
-}
+
+
+
+
+
+
+    class Studentinformation
+    {
+
+
+        public string name;
+
+        public double GPA;
+        int attendance;
+        public Studentinformation(string aname, double cGPA, int dattendance)
+
+        {
+            name = aname;
+
+            GPA = cGPA;
+            attendance = dattendance;
+        }
+
+
+    }
+   
+    }
+
